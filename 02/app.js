1021c1070a2fc7f09e1165b20205c4ede5dd98a0
Neil Gorski <neil.gorski@hotmail.ch>
2025-08-07 23:14:50 +0200

    02-done

--- 02/app.js
+++ 02/app.js
@@ -1,28 +1,44 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    const clickEl = document.querySelector('.error--click');
-    const enterEl = document.querySelector('.error--enter');
+  const clickEl = document.querySelector(".error--click");
+  const enterEl = document.querySelector(".error--enter");
+  const alertEl = document.querySelector(".alert");
 
-    setRandomPosition(clickEl);
-    setRandomPosition(enterEl);
+  setRandomPosition(clickEl);
+  setRandomPosition(enterEl);
 
-    initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
-    initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
-
+  initEventWithError(clickEl, "click", new RangeError("Błąd zakresu!"));
+  initEventWithError(enterEl, "mouseenter", new TypeError("Błąd typu!"));
+  alertEl.addEventListener("click", (e) => {
+    if (e.target.classList.contains("alert")) {
+      e.target.classList.add("alert--hidden");
+    }
+  });
 }
 
 function setRandomPosition(element, error = null) {
-    element.style.top = Math.random() * 600 + 'px';
-    element.style.left = Math.random() * 800 + 'px';
+  element.style.top = Math.random() * 600 + "px";
+  element.style.left = Math.random() * 800 + "px";
 
-    if(error) {
-        throw error;
-    }
+  if (error) {
+    throw error;
+  }
 }
 
 function initEventWithError(element, eventName, error) {
-    element.addEventListener(eventName, function() {
-        setRandomPosition(this, error);
-    })
+  element.addEventListener(eventName, function () {
+    try {
+      setRandomPosition(this, error);
+    } catch (error) {
+      errorHandling(error);
+    }
+  });
+}
+
+function errorHandling(error) {
+  const alertEl = document.querySelector(".alert");
+  const alertMsg = alertEl.querySelector(".alert__message");
+  alertMsg.innerText = error.message;
+  alertEl.classList.remove("alert--hidden");
 }
