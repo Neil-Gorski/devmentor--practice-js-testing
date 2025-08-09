eff17dfc17d78aa73e5f36d5276adc6a1a534ed9
Neil Gorski <neil.go90@gmail.com>
2025-08-09 14:43:32 +0200

    04 - 2 tests done

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,19 @@
 export default class User {
+  constructor(obj) {
+    this.email = this._isValidEmail(obj.email);
+    this.password = obj.password;
+  }
+  getEmail() {
+    return this.email;
+  }
 
+  getPassword() {
+    return this.password;
+  }
+  _isValidEmail(email) {
+    if (email.split("@").length === 1) {
+      throw new Error(`Email: ${email} is invalid`);
+    }
+    return email;
+  }
 }
