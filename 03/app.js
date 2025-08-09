1e6a346ccb59cfb83efba82ecd5d56e49796074c
Neil Gorski <neil.go90@gmail.com>
2025-08-09 13:47:20 +0200

    03 - app.js > create func checkArgType()

--- 03/app.js
+++ 03/app.js
@@ -4,3 +4,9 @@ export default function randomNumber(min, max) {
   }
   return Math.random();
 }
+
+function checkArgType(arg, type) {
+  if (typeof arg !== type) {
+    throw new Error(`Arg: ${arg} has not the type of ${type}`);
+  }
+}
