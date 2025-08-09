d65865f6c3f606686dee29b9921e6b4e871fb5be
Neil Gorski <neil.go90@gmail.com>
2025-08-09 14:53:27 +0200

    04 - 3rd test done

--- 04/User.js
+++ 04/User.js
@@ -1,7 +1,7 @@
 export default class User {
   constructor(obj) {
     this.email = this._isValidEmail(obj.email);
-    this.password = obj.password;
+    this.password = this._isValidPassword(obj.password);
   }
   getEmail() {
     return this.email;
@@ -15,5 +15,14 @@ export default class User {
       throw new Error(`Email: ${email} is invalid`);
     }
     return email;
+  }
+  _isValidPassword(password) {
+    const passwordLengh = password.length;
+    if (passwordLengh < 8) {
+      throw new Error(
+        `This password has only ${passwordLengh} character. Min 8 requierd!`
+      );
+    }
+    return password;
   }
 }
